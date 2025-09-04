c00b5794f721ad89638ad7d6af75b6dcfb05a2d2
Devon Butler <me@devonbutler.net>
2025-09-03 21:05:54 -0400

    add default view for entrys

--- evil/views.py
+++ evil/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request): 
   return HttpResponse("Hello world")
+
+def detail(request, evilentity_id):
+  return HttpResponse("You're looking at entity %s." % evilentity_id)
