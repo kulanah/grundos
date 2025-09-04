47ca7f2faedb55c5de2140c63be8998b662ce575
Devon Butler <me@devonbutler.net>
2025-09-03 21:42:03 -0400

    rename EvilEntry to EvilEntity for clarity

--- evil/models.py
+++ evil/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class EvilEntry(models.Model):
+class EvilEntity(models.Model):
   entity_name = models.CharField(max_length=30)
   text_entry = models.CharField(max_length=1000)
