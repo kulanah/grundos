08496522dc9a0d0cbda42984a25bdeebd22ed7fe
Devon Butler <me@devonbutler.net>
2025-09-05 19:23:06 -0400

    fix save location of image files

--- evil/models.py
+++ evil/models.py
@@ -3,4 +3,4 @@ from django.db import models
 class EvilEntity(models.Model):
   entity_name = models.CharField(max_length=30)
   text_entry = models.CharField(max_length=1000)
-  image = models.ImageField(upload_to="media/entity_image", default='media\default.png')
+  image = models.ImageField(upload_to="entity_image", default='media\default.png')
