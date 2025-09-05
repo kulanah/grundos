2ccc2b4cc0f9ee1a08be1c585d167d798e8eb3a3
Devon Butler <me@devonbutler.net>
2025-09-05 19:17:23 -0400

    update root of evil to have index template that allows user to click into the book

--- evil/views.py
+++ evil/views.py
@@ -5,9 +5,8 @@ from django.shortcuts import get_object_or_404, render
 from .models import EvilEntity
 
 def index(request): 
-  return HttpResponse("Hello world")
+  return render(request, "evil/index.html")
 
 def showcreature(request, evilentity_id):
-  template = loader.get_template("evil/showcreature.html")
   entity = get_object_or_404(EvilEntity, pk=evilentity_id)
   return render(request, "evil/showcreature.html", {"entity": entity})
