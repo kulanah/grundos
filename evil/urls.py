da53200fb7b5b9167703362e2003ecb463529736
Devon Butler <me@devonbutler.net>
2025-09-05 19:18:02 -0400

    update evil urls to display both index and the showcreature page

--- evil/urls.py
+++ evil/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.index, name="index"),
- path("<int:evilentity_id>/", views.showcreature, name="showcreature")]
+urlpatterns = [
+  path("", views.index, name="index"),
+  path("showcreature/<int:evilentity_id>/", views.showcreature, name="showcreature")
+ ]
