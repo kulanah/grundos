311f678ad04cd0fe5cfb3b885c59a56f24b1bb13
Devon Butler <me@devonbutler.net>
2025-09-06 14:43:28 -0400

    add alt text to images in model

--- evil/models.py
+++ evil/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class EvilEntity(models.Model):
   entity_name = models.CharField(max_length=30)
   text_entry = models.CharField(max_length=1000)
+  image_alt_text = models.CharField(max_length=100)
   image = models.ImageField(upload_to="entity_image", default='media\default.png')
