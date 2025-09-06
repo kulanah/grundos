d8fff895fe58bfcba6ff6ba0ec48fbd329faa039
Devon Butler <me@devonbutler.net>
2025-09-06 12:33:57 -0400

    Add the creature list dropdown

--- evil/views.py
+++ evil/views.py
@@ -1,5 +1,3 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
 from .models import EvilEntity
@@ -9,4 +7,12 @@ def index(request):
 
 def showcreature(request, evilentity_id):
   entity = get_object_or_404(EvilEntity, pk=evilentity_id)
-  return render(request, "evil/showcreature.html", {"entity": entity})
+  entityList = EvilEntity.objects.order_by("id")
+  return render(
+    request, 
+    "evil/showcreature.html", 
+    {
+      "entity": entity, 
+      "entityList": entityList
+    }
+  )
