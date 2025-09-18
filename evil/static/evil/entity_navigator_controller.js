bcdca567de4cfbfa1ee908f9be4904f0d04c19d0
Devon Butler <me@devonbutler.net>
2025-09-17 20:15:36 -0400

    Match up entity navigator controller js file in all places

--- evil/static/evil/entity_navigator_controller.js
+++ evil/static/evil/entity_navigator_controller.js
@@ -1,5 +1,6 @@
 const dropdown = document.getElementById('entity_selector');
 const navigateToSelectedEntity = (e) => {
+  console.log(e);
   window.location.href = '/evil/showcreature/' + e.target.value;
 };
 
